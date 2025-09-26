11c3b14da1191f9be48e494389c94fbf74405ca6
Yasmin da Silva Vitoriano Medeiros <20231041110021@ifrn.local>
2025-09-26 17:36:09 -0300

    QUESTOES REFORÇO

---
+++ Q3.py
@@ -0,0 +1,38 @@
+#QUESTAO 3
+#Celsius para Fahrenheit => (celsius * 9/5) + 32
+#Fahrenheit para celsius => (fahrenheit - 32) * 5/9
+
+
+# valor = int(input('Digite o valor escolhido para a conversão: '))
+# escolha = int(input('Você que converter de °C->F [1] ou F->ºC [2]?'))
+
+# if escolha == 1:
+#     c_f = (valor * 9/5) + 32
+#     print(f'O valor após a conversão para Fahrenheit é {c_f}°F')
+# elif escolha == 2:
+#     f_c = (valor - 32) * 5/9 
+#     print(f'O valor após a conversão para Celsius é {f_c}°C')
+
+
+# # 05 - Uma loja online cobra frete com base na distância até o cliente. Crie um programa que
+# pergunte:
+# ● A distância (em km) até o endereço do cliente
+# ● O peso da encomenda (em kg)
+# E aplique as seguintes regras:
+# ● Se a distância for até 100 km, o frete base é R$ 10,00; senão, é R$ 20,00
+# ● Para cada quilo acima de 5 kg, cobra-se R$ 2,00 adicionais
+# Ao final, exiba o valor total do frete.
+
+distancia = float(input('Distância até a sua casa(km):  '))
+peso = float(input('Peso da encomenda(kg): '))
+fragil = input('O produto é frágil? [S/N]')
+perecivel
+
+if 
+            
+
+      
+
+
+
+
